e3a9a8807f0bbf0e7113a07cff92b13cb0455215
Shaghayegh Cloudtek <cloudtek.shaghayegh@gmail.com>
2021-04-11 12:54:07 +0430

    Matrix Adjancency Added

--- graph.py
+++ graph.py
@@ -1,4 +1,18 @@
 import pandas as pd
+import networkx
 
-dataframe = pd.read_csv('./soc-sign-bitcoinotc.csv', names=['Source','Destination','Weight', 'Time'], usecols=[0,1,2], header=0)
-print(dataframe)
+data = pd.read_csv('./soc-sign-bitcoinotc.csv', names=['Source','Destination','Weight', 'Time'], usecols=[0,1,2])
+print(data)
+
+
+edgeList = data.values.tolist()
+print(edgeList[0][0], edgeList[0][1], edgeList[0][2])
+G = networkx.DiGraph()
+
+for i in range(len(edgeList)):
+    G.add_edge(edgeList[i][0], edgeList[i][1], weight=edgeList[i][2])
+
+print(G)
+A = networkx.adjacency_matrix(G).A
+
+print(A)
