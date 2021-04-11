d809b0896c69bcc58b317116783ac6929132289b
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-04-11 12:38:09 +0430

    Header option was deleted

--- graph.py
+++ graph.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-dataframe = pd.read_csv('./soc-sign-bitcoinotc.csv', names=['Source','Destination','Weight', 'Time'], usecols=[0,1,2], header=0)
+dataframe = pd.read_csv('./soc-sign-bitcoinotc.csv', names=['Source','Destination','Weight', 'Time'], usecols=[0,1,2])
 print(dataframe)
