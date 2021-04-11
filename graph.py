8ad8d29ee6933d0e19b896347dd27c7d9598f9d5
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-04-11 13:09:03 +0430

    Get degrees was added

--- graph.py
+++ graph.py
@@ -3,18 +3,27 @@ import networkx
 
 #Load data
 data = pd.read_csv('./soc-sign-bitcoinotc.csv', names=['Source','Destination','Weight', 'Time'], usecols=[0,1,2])
+print("######### Loaded Data #########")
 print(data)
+print("###############################")
 
 #Adjency matrix
 edgeList = data.values.tolist()
-print(edgeList[0][0], edgeList[0][1], edgeList[0][2])
 G = networkx.DiGraph()
 
 for i in range(len(edgeList)):
     G.add_edge(edgeList[i][0], edgeList[i][1], weight=edgeList[i][2])
 
-print(G)
 A = networkx.adjacency_matrix(G).A
 
+print("######### Adjency Matrix #########")
 print(A)
+print("##################################")
 
+
+
+degrees = [val for (node, val) in G.degree()]
+
+print("######### Degrees #########")
+print(A)
+print("###########################")
