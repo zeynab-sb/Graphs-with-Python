17fe5acd489fdce2d026f77b83db5ec0226f3433
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-04-17 10:24:38 +0430

    Graph created in propagation

--- propagation.py
+++ propagation.py
@@ -7,3 +7,9 @@ data = pd.read_csv('./soc-sign-bitcoinotc.csv', names=['Source','Destination','W
 print("######### Loaded Data #########")
 print(data)
 print("###############################")
+
+edgeList = data.values.tolist()
+G = networkx.DiGraph()
+
+for i in range(len(edgeList)):
+    G.add_edge(edgeList[i][0], edgeList[i][1], weight=edgeList[i][2])
